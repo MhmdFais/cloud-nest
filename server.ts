658f789a4658516a7c53c13febdfe8b0e14faf72
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-10-01 22:51:54 +0530

    Update package.json and tsconfig.json

--- server.ts
+++ server.ts
@@ -14,6 +14,7 @@ const registerRoute = require("./routes/register");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
